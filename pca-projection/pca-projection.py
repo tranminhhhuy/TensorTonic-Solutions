e2bc1b1bc632fc634c756d5cfad830cd64460222
tranminhhhuy <165127339+tranminhhhuy@users.noreply.github.com>
2026-06-03 14:46:16 +0700

    Update solution for pca-projection

--- pca-projection/pca-projection.py
+++ pca-projection/pca-projection.py
@@ -4,26 +4,27 @@ def pca_projection(X, k):
     """
     Project data onto the top-k principal components.
     """
-    X=np.array(X)
-    u=X[0]
-    result=[]
-    # Center data find
-    for i  in range (len(X) -1 ):
-        u=u+X[i+1]
+    # Write code here
     
-    u=u/len(X)
-    Center_DaTa= X-u
-    Center_DaTa_Transpose=np.transpose(Center_DaTa)
-    # find covariance of data 
-    cov_DaTa=np.dot(Center_DaTa_Transpose,Center_DaTa)/len(X)
-            
-    # find eigenvector dont change direction when we scale it with number and eigenvalue to arrange to find principal component according in order to  decending 
-    eigenvalues ,eigenvectors =np.linalg.eig(cov_DaTa) 
-    idx= np.argsort(eigenvalues)[::-1]
-    eigvals_sorted = eigenvalues[idx]# data set we have base on to select principal components 
-    eigenvectors_sorted=eigenvectors[:,idx]
     
-    w=eigenvectors_sorted[:, : k]
-    result= Center_DaTa @ w
-
-    return (result[:, : k])
+    
+    # Write code here
+    #  sum of each component vector and divide by the number of samples to get the mean
+    X_mean = np.mean(X, axis=0)
+    X_meaned = X - X_mean
+    #compute variance matrix 
+    cov_mat= np.cov(X_meaned, rowvar=False) 
+    # like make make matrix transpose and then dot product with original matrix
+    # find principal components  , The eigenvectors are the principal component directions.
+    eigenvalues, eigenvectors = np.linalg.eig(cov_mat)
+    # sort the eigenvalues and eigenvectors in descending order
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    #  sot the eigenvalues in descending order and get the indices    
+    sorted_eigenvalues = eigenvalues[sorted_indices]#using index for sorted eigevalues 
+    sorted__eigenvectors = eigenvectors[:, sorted_indices ] #using index for sorted eigenvectors
+    # select the top-k eigenvectors
+    eigenvector_subset = sorted__eigenvectors[:, 0:k] 
+    # select the top-k eigenvectors
+    # project the data onto the top-k eigenvectors 
+    x_reduceced=np.dot(eigenvector_subset.T,X_meaned.T).T # project the data onto the top-k eigenvectors
+    return x_reduceced
