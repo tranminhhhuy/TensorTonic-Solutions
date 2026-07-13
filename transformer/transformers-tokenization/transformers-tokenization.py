ab4abe27aa6d16fad0c89119cb535bac6cdba43b
tranminhhhuy <165127339+tranminhhhuy@users.noreply.github.com>
2026-07-13 15:35:22 +0700

    Solve transformers-tokenization

--- transformer/transformers-tokenization/transformers-tokenization.py
+++ transformer/transformers-tokenization/transformers-tokenization.py
@@ -7,7 +7,7 @@ class SimpleTokenizer:
     """
     
     def __init__(self):
-        self.word_to_id: Dict[str, int] = {}
+        self.word_to_id: Dict[str, int] = {} # self.word to id [str -> int] example : {"hello": 4, "world": 5}
         self.id_to_word: Dict[int, str] = {}
         self.vocab_size = 0
         
@@ -17,37 +17,33 @@ class SimpleTokenizer:
         self.bos_token = "<BOS>"
         self.eos_token = "<EOS>"
     
-    def build_vocab(self, texts: List[str]) -> None:
+    def build_vocab(self, texts: List[str]) -> None: # stag2 
         """
         Build vocabulary from a list of texts.
         Add special tokens first, then unique words.
         """
-
-        self.word_to_id[self.pad_token]=0
-        self.word_to_id[self.unk_token]=1
-        self.word_to_id[self.bos_token]=2
-        self.word_to_id[self.eos_token]=3
-
-        self.id_to_word[0]=self.pad_token
-        self.id_to_word[1]=self.unk_token
-        self.id_to_word[2]=self.bos_token
-        self.id_to_word[3]=self.eos_token
-
-        sorted_word=set()
-        idx= 4
-        for text in texts:
-            word=text.lower().split()
-            sorted_word.update(word)
-        sorted_word=sorted(sorted_word)
-        for words in sorted_word:
-            if words not in self.word_to_id:
-                self.word_to_id[words]=idx
-                self.id_to_word[idx]=words
-                idx+=1
-        
-        self.vocab_size = len(self.word_to_id)
-        
         # YOUR CODE HERE
+        # add special tokens
+        special_tokens = [self.pad_token, self.unk_token, self.bos_token, self.eos_token]
+        for token in special_tokens:   
+            self.word_to_id[token]=self.vocab_size
+            self.id_to_word[self.vocab_size]=token
+            self.vocab_size+=1
+
+        # add unique words 
+        unique_words = set()
+        for text in texts:
+            words= text.split()
+            unique_words.update(words)
+        unique_words=sorted(unique_words)# sorted to ensure consistent ordering
+
+        for word in unique_words:
+            self.word_to_id[word]=self.vocab_size
+            self.id_to_word[self.vocab_size]=word
+            self.vocab_size+=1
+
+
+
         pass
     
     def encode(self, text: str) -> List[int]:
@@ -55,16 +51,17 @@ class SimpleTokenizer:
         Convert text to list of token IDs.
         Use UNK for unknown words.
         """
-        texts=text.lower().split()
-        token_id=[]
-        for word in texts :
-            if word not in self.word_to_id:
-                token_id.append(self.word_to_id[self.unk_token])
-            else:
-                token_id.append(self.word_to_id[word])
-                
-        return token_id
+
         # YOUR CODE HERE
+        text=text.lower()
+        tokens=text.split()
+        token_ids=[]
+        for token in tokens:
+            if token in  self.word_to_id:
+                token_ids.append(self.word_to_id[token])
+            else:
+                token_ids.append(self.word_to_id[self.unk_token])
+        return token_ids
         pass
     
     def decode(self, ids: List[int]) -> str:
@@ -72,11 +69,11 @@ class SimpleTokenizer:
         Convert list of token IDs back to text.
         """
         # YOUR CODE HERE
-        words= []
+        words=[]
         for id in ids:
-            if id not in self.id_to_word:
-                words.append(self.unk_token)
-            else: 
+            if id in self.id_to_word:
                 words.append(self.id_to_word[id])
-        return " ".join(words)
-        pass
+            else:
+                words.append(self.unk_token)
+        return ' '.join(words)
+    pass
