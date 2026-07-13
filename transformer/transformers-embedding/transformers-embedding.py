d17834057efff3ff311c561f660a97ec689ffcd8
tranminhhhuy <165127339+tranminhhhuy@users.noreply.github.com>
2026-07-13 17:28:38 +0700

    Solve transformers-embedding

--- transformer/transformers-embedding/transformers-embedding.py
+++ transformer/transformers-embedding/transformers-embedding.py
@@ -7,14 +7,16 @@ def create_embedding_layer(vocab_size: int, d_model: int) -> nn.Embedding:
     Create an embedding layer.
     """
     # Your code here
-    return nn.Embedding(vocab_size,d_model)
-    
-    pass
+    embedding_matrix = nn.Embedding(vocab_size, d_model)
+    # Initialize the embedding matrix with random values
+    nn.init.uniform_(embedding_matrix.weight, -0.6, 0.6)# number create random values between -0.1 and 0.1
+    return embedding_matrix
 
 def embed_tokens(embedding: nn.Embedding, tokens: torch.Tensor, d_model: int) -> torch.Tensor:
     """
     Convert token indices to scaled embeddings.
     """
     # Your code here
-    return embedding(tokens)*math.sqrt(d_model)
+    embeddings= embedding(tokens) * math.sqrt(d_model)
+    return embeddings
     pass
