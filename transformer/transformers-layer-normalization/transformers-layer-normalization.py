28b9067be87c4932b50f9683c048fd7657e2bd36
tranminhhhuy <165127339+tranminhhhuy@users.noreply.github.com>
2026-08-12 20:16:59 +0700

    Solve transformers-layer-normalization

--- transformer/transformers-layer-normalization/transformers-layer-normalization.py
+++ transformer/transformers-layer-normalization/transformers-layer-normalization.py
@@ -5,11 +5,14 @@ def layer_norm(x: np.ndarray, gamma: np.ndarray, beta: np.ndarray, eps: float = 
     Returns: Normalized array of same shape as x
     """
     # Your code here
-    mean= np.sum(x, axis=-1, keepdims=True) / x.shape[-1]# compute mean data 
-    var=np.sum(x**2, axis=-1 ,keepdims=True)/ x.shape[-1]-mean **2# compute variacnce data 
-    normalize=(x-mean)/np.sqrt(var+eps)# compute  normalize data 
-    out= gamma*normalize+beta# compute output data
-    return out 
 
-
-    pass
+    # compute mean across all dimension features:
+    x_mean =np.mean(x,axis=-1, keepdims=True)
+    # compute variance across all dimension features:
+    x_var=np.var(x,axis=-1,keepdims=True)
+    # compute normalized x:
+    x_normalized =(x-x_mean)/np.sqrt(x_var+eps)
+    # scale and shift:
+    out= gamma * x_normalized + beta
+    return out
+pass
